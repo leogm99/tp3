8ae233413d2ef976ae75bdf8830e28740f40fcfe
leogm99 <lgiampieri62@gmail.com>
2021-05-20 17:06:30 -0300

    Serializador de comandos del cliente, faltan excepciones

---
+++ client_Command.cpp
@@ -0,0 +1,99 @@
+//
+// Created by leogm99 on 20/5/21.
+//
+
+#include "client_Command.h"
+#include <iostream>
+#include <sstream>
+
+Command::Command() {
+    // el de este namespace por favorcito
+    validCommands = std::move(Command::createMap());
+}
+
+void Command::serializeCommand() {
+   std::string in;
+   std::getline(std::cin, in);
+   std::stringstream ss(in);
+   std::string stringCommand;
+
+   ss >> stringCommand;
+   uint8_t command;
+   try{
+       command = validCommands.at(stringCommand);
+       dispatch(command, ss);
+   } catch(const std::exception& e){
+       std::cout << e.what();
+   }
+}
+
+void Command::dispatch(uint8_t command, std::stringstream& ss) {
+    switch(command) {
+        case crear:
+            create(ss);
+            break;
+        case listar:
+            list();
+            break;
+        case unirse:
+            join(ss);
+            break;
+        case jugar:
+            play(ss);
+            break;
+        default:
+            break;
+    }
+}
+
+void Command::create(std::stringstream& ss) {
+    actualCommand.clear();
+    actualCommand.emplace_back(crear);
+    std::string game;
+    ss >> game;
+    actualCommand.emplace_back(game.size());
+    std::copy(game.begin(), game.end(), std::back_inserter(actualCommand));
+}
+
+void Command::list() {
+    actualCommand.clear();
+    actualCommand.emplace_back(listar);
+}
+
+void Command::join(std::stringstream& ss) {
+    actualCommand.clear();
+    actualCommand.emplace_back(unirse);
+    std::string game;
+    ss >> game;
+    actualCommand.emplace_back(game.size());
+    std::copy(game.begin(), game.end(), std::back_inserter(actualCommand));
+}
+
+void Command::play(std::stringstream& ss) {
+    actualCommand.clear();
+    actualCommand.emplace_back(jugar);
+    std::string row;
+    std::string col;
+    ss >> col;
+    ss >> row;
+    uint8_t colN = std::stoul(col, nullptr, 0);
+    uint8_t rowN = std::stoul(row, nullptr, 0);
+    --colN; // required by solution
+    --rowN;
+
+    colN <<= 4;
+    uint8_t colRow = colN | rowN;
+    actualCommand.emplace_back(colRow);
+}
+
+Command::~Command() {
+}
+
+Command::Command(Command &&other) noexcept
+: actualCommand(std::move(other.actualCommand)){
+}
+
+Command &Command::operator=(Command &&other) noexcept {
+    actualCommand = std::move(other.actualCommand);
+    return *this;
+}
---
+++ client_Command.h
@@ -0,0 +1,51 @@
+//
+// Created by leogm99 on 20/5/21.
+//
+
+#ifndef TP3_CLIENT_COMMAND_H
+#define TP3_CLIENT_COMMAND_H
+#include <map>
+#include <vector>
+
+static const uint8_t crear = 0x6e;
+static const uint8_t listar = 0x6c;
+static const uint8_t unirse = 0x6a;
+static const uint8_t jugar = 0x70;
+
+class Command {
+private:
+    static std::map<std::string, uint8_t>
+    createMap(){
+        std::map<std::string, uint8_t> commands;
+        commands["crear"] = crear;
+        commands["listar"] = listar;
+        commands["unirse"] = unirse;
+        commands["jugar"] = jugar;
+        return commands;
+    }
+
+    // command value serialization
+    std::map<std::string, uint8_t> validCommands;
+    std::vector<uint8_t> actualCommand;
+
+    // methods that serialize the interpreted command
+    void dispatch(uint8_t command, std::stringstream& ss);
+    void create(std::stringstream& ss);
+    void list();
+    void join(std::stringstream& ss);
+    void play(std::stringstream& ss);
+public:
+    Command();
+    Command(const Command& other) = delete;
+    Command& operator=(const Command& other) = delete;
+
+    Command(Command&& other) noexcept;
+    Command& operator=(Command&& other) noexcept;
+
+    void serializeCommand();
+    ~Command();
+};
+
+
+
+#endif //TP3_CLIENT_COMMAND_H
