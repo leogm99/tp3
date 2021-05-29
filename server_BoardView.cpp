b0b33475fe78a4fd065b6431c17f2b01212ec17a
leogm99 <lgiampieri62@gmail.com>
2021-05-29 01:17:56 -0300

    Reemplazo el archivo del tablero por un string, sercom no me funcaba

--- server_BoardView.cpp
+++ server_BoardView.cpp
@@ -1,15 +1,18 @@
-//
-// Created by leogm99 on 19/5/21.
-//
-
 #include "server_BoardView.h"
 #include <string>
 #include <utility>
-
+#include <map>
 
 BoardView::BoardView()
-: fh(BoardFileHandler("board.txt")){
-    boardView = std::move(fh.getPlainBoard());
+: boardView("    1 . 2 . 3 .\n"
+            "  +---+---+---+\n"
+            "1 |   |   |   |\n"
+            "  +---+---+---+\n"
+            "2 |   |   |   |\n"
+            "  +---+---+---+\n"
+            "3 |   |   |   |\n"
+            "  +---+---+---+\n"),
+  boardMapping(std::move(createMap())){
 }
 
 const std::string& BoardView::print() {
@@ -17,21 +20,25 @@ const std::string& BoardView::print() {
 }
 
 void BoardView::updateAt(unsigned int col, unsigned int row, char symbol) {
-    unsigned int mapping = fh.getMapping(col, row);
+    unsigned int mapping = getMapping(col, row);
     boardView[mapping] = symbol;
 }
 
 BoardView::BoardView(BoardView &&other) noexcept
-: fh(std::move(other.fh)) {
-    boardView = fh.getPlainBoard();
+: boardView(std::move(other.boardView)),
+  boardMapping(std::move(other.boardMapping)){
 }
 
 BoardView &BoardView::operator=(BoardView &&other) noexcept {
     if (this == &other){
         return *this;
     }
-    fh = std::move(other.fh);
     boardView = std::move(other.boardView);
+    boardMapping = std::move(other.boardMapping);
     return *this;
 }
 
+unsigned int BoardView::
+getMapping(unsigned int col, unsigned int row) const {
+    return boardMapping.at(std::make_pair(col, row));
+}
--- server_BoardView.h
+++ server_BoardView.h
@@ -1,18 +1,36 @@
-//
-// Created by leogm99 on 19/5/21.
-//
-
 #ifndef TP3_SERVER_BOARDVIEW_H
 #define TP3_SERVER_BOARDVIEW_H
 
 #include "server_BoardFileHandler.h"
 #include <iostream>
 #include <string>
+#include <utility>
+#include <map>
 
 class BoardView {
 private:
     std::string boardView;
-    BoardFileHandler fh;
+
+    static std::map<std::pair<unsigned int, unsigned int>,
+            unsigned int> createMap(){
+        std::map<std::pair<unsigned int, unsigned int>,
+                unsigned int> mapping;
+        mapping[std::make_pair(1, 1)] = 0x24;
+        mapping[std::make_pair(1, 2)] = 0x44;
+        mapping[std::make_pair(1, 3)] = 0x64;
+        mapping[std::make_pair(2, 1)] = 0x28;
+        mapping[std::make_pair(2, 2)] = 0x48;
+        mapping[std::make_pair(2, 3)] = 0x68;
+        mapping[std::make_pair(3, 1)] = 0x2c;
+        mapping[std::make_pair(3, 2)] = 0x4c;
+        mapping[std::make_pair(3, 3)] = 0x6c;
+        return mapping;
+    }
+    std::map<std::pair<unsigned int, unsigned int>, unsigned int> boardMapping;
+
+    unsigned int getMapping(unsigned int col,
+                            unsigned int row) const;
+
 public:
     BoardView();
 
